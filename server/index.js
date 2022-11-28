a5f88fec7a01a08bdba0c20046b7baf95f11a3b0
Yuvraj Singh <89746017+YuviSingh18@users.noreply.github.com>
2022-11-28 08:20:58 -0500

    server built

--- server/index.js
+++ server/index.js
@@ -4,12 +4,42 @@ const app = express()
 const hostname = '127.0.0.1';
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Beautiful World !');
-});
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 
-app.listen(port, hostname, () => {
+})
+
+app.use('/', express.static('./client/dist'));
+
+app.use(express.json());
+
+app
+.get('/', (req, res) => {
+  res.status(200).send('Happy Sweet New Year');
+})
+.get('/error', (req, res) => {
+  sss.PORT();
+})
+
+
+
+app.get('*', (req, res) => {
+    
+  res.sendFile('index.html', {root: './client/dist'});
+})
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status( err.httpCode ?? 500).send({
+      message: err.message ?? 'Something went wrong',
+      status: err.httpCode ?? 500
+  });
+
+})
+
+app.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
